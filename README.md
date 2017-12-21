1e2d83eadb01c273fb666449d028cef5a74cb7aa
Alok Singh <singhalok641@gmail.com>
2017-12-21 16:31:06 +0530

    added login functoionality

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# Backend
+# Node.js login for modstore
+
+### Version
+1.1.0
+
+### Usage
+
+
+### Installation
+
+Loginapp requires [Node.js](https://nodejs.org/) v4+ to run.
+
+```sh
+$ npm install
+```
+
+```sh
+$ npm start
+```
--- app.js
+++ app.js
@@ -1,33 +1,87 @@
 var express = require('express');
-var app = express();
+var path = require('path');
+var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var exphbs = require('express-handlebars');
+var expressValidator = require('express-validator');
+var flash = require('connect-flash');
+var session = require('express-session');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+var mongo = require('mongodb');
 var mongoose = require('mongoose');
 
-Account = require('./models/accounts');
-//Connect to mongoose
-mongoose.connect('mongodb://localhost:27017/modstores');
+mongoose.connect('mongodb://localhost/loginapp');
 var db = mongoose.connection;
 
-db.on('error', err => {
-  console.error(`Error while connecting to DB: ${err.message}`);
+var routes = require('./routes/index');
+var users = require('./routes/users');
+
+// Init App
+var app = express();
+
+// View Engine
+app.set('views', path.join(__dirname, 'views'));
+app.engine('handlebars', exphbs({defaultLayout:'layout'}));
+app.set('view engine', 'handlebars');
+
+// BodyParser Middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+// Set Static Folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Express Session
+app.use(session({
+    secret: 'secret',
+    saveUninitialized: true,
+    resave: true
+}));
+
+// Passport init
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Express Validator
+app.use(expressValidator({
+  errorFormatter: function(param, msg, value) {
+      var namespace = param.split('.')
+      , root    = namespace.shift()
+      , formParam = root;
+
+    while(namespace.length) {
+      formParam += '[' + namespace.shift() + ']';
+    }
+    return {
+      param : formParam,
+      msg   : msg,
+      value : value
+    };
+  }
+}));
+
+// Connect Flash
+app.use(flash());
+
+// Global Vars
+app.use(function (req, res, next) {
+  res.locals.success_msg = req.flash('success_msg');
+  res.locals.error_msg = req.flash('error_msg');
+  res.locals.error = req.flash('error');
+  res.locals.user = req.user || null;
+  next();
 });
 
-app.get('/', function(req, res){
-	res.send("Please use /api/modstores");
+
+
+app.use('/', routes);
+app.use('/users', users);
+
+// Set Port
+app.set('port', (process.env.PORT || 3002));
+
+app.listen(app.get('port'), function(){
+	console.log('Server started on port '+app.get('port'));
 });
-
-app.get('/api/accounts/', function(req, res){
-	Account.getAccounts(function(err, account){
-		if(err){
-			throw err;
-		}
-		console.log(account);
-		res.json(account);
-	});
-});
-
-app.listen(3000);
-console.log('Running on port 3000....');
-
-
-
